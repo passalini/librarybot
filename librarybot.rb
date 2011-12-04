b3924a75d6bdbed0e33fa3308bca9f79b2ce237f
Thawan Fidelis <thawan.fidelis@gmail.com>
2011-12-03 23:11:25 -0200

    Modelagem do problema usando conceitos de OO, cada livro/aluno e um objeto

--- librarybot.rb
+++ librarybot.rb
@@ -1,39 +1,50 @@
-# -*- coding: utf-8 -*-
 require 'capybara'
+require 'nokogiri'
 
-class Bot
-  attr_accessor :session, :objeto_procurado
-  def initialize(*params)
-    Capybara.app_host = 'http://www.bibliotecas.uenf.br/informa/cgi-bin/biblio.dll/emprest?g=geral&bd=&p=GERAL'
-    @session = Capybara::Session.new(:selenium)
-    @session.visit('/')
-  end
-
-  def preencher(matricula, senha)
-    @session.fill_in 'matricula', with: matricula
-    @session.fill_in 'senha', with: senha
-    @session.click_button 'Consultar'
-  end
-
-  def contem_livro?(livro)
-    lista_de_objetos = @session.all('td', text: 'Renovação')
-
-    lista_de_objetos.each do |objeto|
-      if objeto.has_content? livro and objeto.has_link? :anyone
-        @objeto_procurado = objeto
-        return true
-      end
+class Livro
+    def initialize(codigo, data_devolucao, link_para_renovar)
+        @codigo, @data_devolucao, @link_para_renovar = codigo, data_devolucao, link_para_renovar
     end
-  end
 
-  def renovar(livro)
-    if contem_livro? livro
-      @objeto_procurado.find('a', :text => 'Renovação').click
-      if @objeto_procurado.has_content? "Publicação não renovada"
-        return "Publicação não renovada"
-      else
-        return "Renovado"
-      end
-    end
-  end
+    attr_accessor :codigo, :data_devolucao, :link_para_renovar
+
 end
+
+class Aluno
+    def initialize(matricula, senha)
+        @matricula, @senha = matricula, senha
+        @livros_em_emprestimo = []
+    end
+
+    attr_accessor :session, :objeto_procurado
+
+    def visitar(site)
+        Capybara.app_host = site
+        @session = Capybara::Session.new(:selenium)
+    end
+
+    def livros_em_emprestimo
+
+        doc = Nokogiri::HTML(@session.html)
+        informacoes_de_livros = doc.xpath('//p')
+        links = doc.xpath('//a')
+        for livro in 1..(informacoes_de_livros.length) -1
+            @livros_em_emprestimo << Livro.new( informacoes_de_livros[livro].to_str.split.first ,
+                                                informacoes_de_livros[livro].to_str.split.last ,
+                                                links[livro].to_s)
+        end
+        @livros_em_emprestimo
+    end
+
+    def logar
+        @session.visit('/')
+        @session.fill_in 'matricula', with: @matricula
+        @session.fill_in 'senha', with: @senha
+        @session.click_button 'Consultar'
+      end
+
+    def renovar(livro)
+        @session.visit(livro.link_para_renovar)
+    end
+end
+
